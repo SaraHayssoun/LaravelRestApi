3c0491db195ffb6602be3055e6d19b34141b569d
SaraHayssoun <sara.hayssoun@isae.edu.lb>
2019-08-16 12:12:39 +0300

    Add Contact Model and Controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Contact::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $contact = new Contact($request->all());
+        $contact->save();
+
+        return Response::json($contact);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Contact::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::find($id);
+        $contact->firstname = $request->firstname;
+        $contact->firstname = $request->lastname;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->save();
+
+        return Response::json($contact);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get(‘/’, function () {
+//    return view(‘index’);
+//});
+
+Route::group([‘prefix’ => ‘api/v1’], function() {
+ Route::resource(‘todos’, ‘ContactController’);
+});
